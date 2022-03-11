c0aec2d3d6133f85abbef4e6c8ae843be5a95a74
wilpet4 <william.petrik@gmail.com>
2022-03-11 13:28:45 +0100

    continued work on DbContext

--- PragueParkingV2/DataAccess/Models/AppContext.cs
+++ PragueParkingV2/DataAccess/Models/AppContext.cs
@@ -1,14 +1,28 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace PragueParkingDataAccess
 {
     public class AppContext : DbContext
     {
+        public AppContext() { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var builder = new ConfigurationBuilder()
+            .AddJsonFile($"appsettings.json", true, true);
+
+            string connectionsString = 
+            builder.Build().GetConnectionString("DefaultConnection");
+
+            if (optionsBuilder.IsConfigured == false)
+            {
+                optionsBuilder.UseSqlServer();
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
         public DbSet<ParkingGarage> Garages { get; set; }
         public DbSet<ParkingSpot> ParkingSpots { get; set; }
         public DbSet<Car> Cars { get; set; }
@@ -28,14 +42,20 @@ namespace PragueParkingDataAccess
     public abstract class Vehicle
     {
         public int Id { get; set; }
-
+        public byte Size { get; set; }
     }
     public class Car : Vehicle
     {
-
+        public Car(in byte size = 4) // ?
+        {
+            Size = size;
+        }
     }
     public class MC : Vehicle
     {
-
+        public MC(in byte size = 2) // ?
+        {
+            Size = size;
+        }
     }
 }
