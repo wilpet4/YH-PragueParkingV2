57152f20186d824d13c4729f93093a202c005a1d
wilpet4 <william.petrik@gmail.com>
2022-03-15 10:55:22 +0100

    database almost finished maybe

---
+++ PragueParkingV2/DataAccess/Migrations/20220315095132_test.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WpfAppDataAccess.Migrations
+{
+    public partial class test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ParkingSpots",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ParkingSpots", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Garages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ParkingSpotsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Garages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Garages_ParkingSpots_ParkingSpotsId",
+                        column: x => x.ParkingSpotsId,
+                        principalTable: "ParkingSpots",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vehicle",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Size = table.Column<byte>(type: "tinyint", nullable: false),
+                    Registration = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ParkingSpotsId = table.Column<int>(type: "int", nullable: false),
+                    Discriminator = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehicle", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Vehicle_ParkingSpots_ParkingSpotsId",
+                        column: x => x.ParkingSpotsId,
+                        principalTable: "ParkingSpots",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Garages_ParkingSpotsId",
+                table: "Garages",
+                column: "ParkingSpotsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vehicle_ParkingSpotsId",
+                table: "Vehicle",
+                column: "ParkingSpotsId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Garages");
+
+            migrationBuilder.DropTable(
+                name: "Vehicle");
+
+            migrationBuilder.DropTable(
+                name: "ParkingSpots");
+        }
+    }
+}
--- PragueParkingV2/DataAccess/Models/AppContext.cs
+++ PragueParkingV2/DataAccess/Models/AppContext.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using System.Collections.Generic;
 
 namespace PragueParkingDataAccess
 {
@@ -11,12 +13,12 @@ namespace PragueParkingDataAccess
             var builder = new ConfigurationBuilder()
             .AddJsonFile($"appsettings.json", true, true);
 
-            string connectionsString = 
+            string connectionString = 
             builder.Build().GetConnectionString("DefaultConnection");
 
             if (optionsBuilder.IsConfigured == false)
             {
-                optionsBuilder.UseSqlServer();
+                optionsBuilder.UseSqlServer(connectionString);
             }
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -32,30 +34,36 @@ namespace PragueParkingDataAccess
     public class ParkingGarage
     {
         public int Id { get; set; }
+        public ParkingSpot ParkingSpots { get; set; }
 
     }
     public class ParkingSpot
     {
         public int Id { get; set; }
-
+        public virtual List<Vehicles> Vehicles { get; set; }
     }
-    public abstract class Vehicle
+    public abstract class Vehicles
     {
         public int Id { get; set; }
         public byte Size { get; set; }
+        public string Registration { get; set; }
     }
-    public class Car : Vehicle
+    public class Car : Vehicles
     {
-        public Car(in byte size = 4) // ?
+        public Car() { }
+        public Car(in byte size = 4, in string reg = "") // ?
         {
             Size = size;
+            Registration = reg;
         }
     }
-    public class MC : Vehicle
+    public class MC : Vehicles
     {
-        public MC(in byte size = 2) // ?
+        public MC() { }
+        public MC(in byte size = 2, in string reg = "") // ?
         {
             Size = size;
+            Registration = reg;
         }
     }
 }
